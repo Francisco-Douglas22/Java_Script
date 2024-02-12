6060e810b3bf511bf7e71e8bef5c3d95e3b2f7cb
Francisco Douglas <franciscodouglas77@outlook.com>
2024-02-12 05:12:10 -0300

    ---------

---
+++ Teste.js
@@ -0,0 +1,11 @@
+var num1 = prompt("Entre com o primeiro numero: ");
+var num2 = prompt("Entre com o segundo numero: ");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+var soma = num1 + num2;
+
+var media = soma / 2;
+
+alert(`A soma e ${soma} e a media e ${media}`);
